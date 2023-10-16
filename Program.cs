7fe082f0f8a8b2fe1dabe20173ae348b191585b0
LetY0uDown <maks.manin.04@gmail.com>
2023-10-16 13:45:30 +1000

    Изменение 1

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
-            int.TryParse(Console.ReadLine(), out int num);
-            int result = CountNumber(num);
+            //int.TryParse(Console.ReadLine(), out int num);
+            //int result = CountNumber(num);
+            //Console.WriteLine(result);
+
+            int[] array = new int[]{1,6,10,12,1,18,2};
+            int result = Min(array, 3);
             Console.WriteLine(result);
         }
         //задача 2
@@ -16,11 +20,27 @@
             else
                 return 1 + CountNumber(n / 10);
         }
-        //задача 4
-        static int Root(int a, int b, int c)
+        //задача 5
+        
+        static int Min(int[] x, int k)
         {
-
+            if(x.Length-1 == k)
+            {
+                return x[k];
+            }
+            int min = Min(x, k + 1);
+            if (x[k] < min)
+            {
+                return x[k];
+            }
+            else 
+                return min;
         }
+        //задача 6
+        //static int Sim(int S, int I, int J)
+        //{
+
+        //}
     }
 
 }
