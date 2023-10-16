9c7773a4961b3ec0cc7ffc07aaba7197185e55a0
LetY0uDown <maks.manin.04@gmail.com>
2023-10-16 13:14:37 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace З.А_задание_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int num);
+            int result = CountNumber(num);
+            Console.WriteLine(result);
+        }
+        //задача 2
+        static int CountNumber(int n)
+        {
+            if (n <= 0)
+                return 0;
+            else
+                return 1 + CountNumber(n / 10);
+        }
+        //задача 4
+        static int Root(int a, int b, int c)
+        {
+
+        }
+    }
+
+}
